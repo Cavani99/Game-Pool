bc396f5e5f1b98fa79b28d9c446887b54b45b487
Cavani99 <kristiian.donchev@abv.bg>
2026-04-20 23:07:44 +0300

    chore: set chat box design

--- src/main/java/project/web/ChatController.java
+++ src/main/java/project/web/ChatController.java
@@ -4,12 +4,18 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import project.model.User;
 import project.security.AuthenticationDetails;
 import project.service.MessageService;
 import project.service.UserService;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/chat")
@@ -33,9 +39,25 @@ public class ChatController {
         User user = userService.getById(userDetails.getId());
 
         modelAndView.addObject("friends", user.getFriends());
-        modelAndView.addObject("page", "friends");
-        modelAndView.addObject("title", "Friends");
+        modelAndView.addObject("page", "chat");
+        modelAndView.addObject("title", "Chat");
 
         return modelAndView;
+    }
+
+    @PostMapping("open_chat/{id}")
+    @PreAuthorize("hasAuthority('USER')")
+    public Map<String, String> getChatForUser(@PathVariable("id") UUID friendId, @AuthenticationPrincipal AuthenticationDetails userDetails, Locale locale) {
+        User user = userService.getById(userDetails.getId());
+        User friend = userService.getById(friendId);
+
+        if (!user.getFriends().contains(friend)) {
+            String message = messageService.getLocalizedMessage("chat_not_friend", locale);
+            return Map.of("error", message);
+        }
+
+        //get messages
+
+        return Map.of("message", "");
     }
 }
